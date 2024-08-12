06e855b95761e2608ee23fe7b97df7ee620f9591
FENRiS738 <as420738@gmail.com>
2024-08-12 23:53:22 +0530

    Studied Linear Regression part 2

--- model_training.py
+++ model_training.py
@@ -10,8 +10,22 @@ data = data[["G1","G2","G3","studytime","failures","absences"]]
 
 predict = "G3"
 
-x = np.array(data.drop([predict], 1))
+x = np.array(data.drop([predict], axis=1))
 y = np.array(data[predict])
 
-x_train, y_train, x_test, y_test = sklearn.model_selection.train_test_split(x, y, train_size=0.1)
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, train_size=0.1)
 
+linear = linear_model.LinearRegression()
+
+linear.fit(x_train, y_train)
+accuracy = linear.score(x_test, y_test)
+
+print(accuracy)
+
+print("Coefficient: \n", linear.coef_)
+print("Intercept: \n", linear.intercept_)
+
+predictions = linear.predict(x_test)
+
+for x in range(len(predictions)):
+    print(round(predictions[x]), x_test[x], y_test[x])
